eea5d5307e9da16c223bba7db2eede427025991f
Fernando Chicaiza <washopilot@yahoo.com>
2022-03-27 18:58:54 -0500

    desgloce de modulos y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, SharedModule, GifsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
