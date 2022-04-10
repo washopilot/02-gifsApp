6cf17397f4a5361542ff0fbc94b20c88fdd87101
Fernando Chicaiza <washopilot@yahoo.com>
2022-04-09 22:22:17 -0500

    animate.css

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -33,7 +33,6 @@ export class GifsService {
     this.http
       .get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params })
       .subscribe((resp) => {
-        console.log(resp.data);
         this.resultados = resp.data;
         localStorage.setItem('resultados', JSON.stringify(this.resultados));
       });
